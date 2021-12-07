05b0c69a24da31688daab3249d514b1657f56abb
Poga Huang <pgh86812@gmail.com>
2021-12-07 20:24:16 +0800

    Initial Commit

---
+++ Homework34Latitude&Longitude/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework34Latitude&Longitude
+//
+//  Created by 黃柏嘉 on 2021/12/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
